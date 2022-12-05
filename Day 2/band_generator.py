8ca0f390ab038b691d9a08fba5165ff52c142072
AamirNawab <79589463+AamirNawab@users.noreply.github.com>
2022-12-05 21:54:16 +0000

    Create band_generator.py

---
+++ Day 2/band_generator.py
@@ -0,0 +1,10 @@
+"""
+Band name generator
+A simple program that ask for user input to combine street name and pet name to create a band name
+
+"""
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
